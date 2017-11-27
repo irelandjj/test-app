cef98cd899138ad8e2403303f28cd2f350c1dd1e
John Jeremy Ireland (JOIR) <joir@oticonmedical.com>
2017-11-27 13:47:28 +0100

    INPC demo

---
+++ TrainingApplication/OneWayDataBinding/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace OneWayDataBinding
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //public static Employee GetEmployee()
+        //{
+        //    var emp = new Employee();
+
+        //    emp.Name = "John";
+        //    emp.Title = "Developer";
+
+        //    return emp;
+        //}
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string caller = "")
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(caller));
+        }
+    }
+    }
+}
---
+++ TrainingApplication/OneWayDataBinding/OneWayDataBindingWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneWayDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Employee emp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            emp = new Employee()
+            {
+                Name = "Jane",
+                Title = "CEO"
+            };
+            DataContext = emp;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            emp.Name = "Mark";
+            emp.Title = "Designer";
+        }
+    }
+}
