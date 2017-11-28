f178e7bd9a39aa6d25b1a8f8652f9199cbf8d8bf
John Jeremy Ireland (JOIR) <joir@oticonmedical.com>
2017-11-28 11:48:50 +0100

    two way data binding

---
+++ TrainingApplication/TwoWayDataBinding/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TwoWayDataBinding
+{
+    public class Employee 
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+            }
+        }
+
+    }
+}
